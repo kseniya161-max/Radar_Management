8584ad66be9b34cec9d1390aade9b2b0ba94d5ae
Kseniya <baharevaxen@yandex.ru>
2026-08-25 11:59:07 +0300

    remove company_to_dict and using schemas

--- app/services/company_service.py
+++ app/services/company_service.py
@@ -42,39 +42,13 @@ async def get_all_companies(
         "total": total,
         "limit": limit,
         "offset": offset,
-        "items": [company_to_dict(company) for company in companies],
+        "items": companies,
     }
 
 
 def update_company_growth(company: Company):
     company.revenue_growth_3 = growth_calc(company.revenue_2025, company.revenue_2024)
     company.profit_growth_3 = growth_calc(company.profit_2025,company.profit_2023)
-
-
-
-def company_to_dict(company: Company) -> dict:
-    return {
-        "id": company.id,
-        "inn": company.inn,
-        "name": company.name,
-        "status": company.status,
-        "okved": company.okved,
-        "revenue_2025": company.revenue_2025,
-        "revenue_2024": company.revenue_2024,
-        "revenue_2023": company.revenue_2023,
-        "profit_2025": company.profit_2025,
-        "profit_2024": company.profit_2024,
-        "profit_2023": company.profit_2023,
-        "revenue_growth": company.revenue_growth_3,
-        "profit_growth": company.profit_growth_3,
-        "phone": company.phone,
-        "email": company.email,
-        "website": company.website,
-        "region": company.region,
-        "registration_date": company.registration_date,
-        "tenders_count": company.tenders_count,
-        "courts_count": company.courts_count,
-    }
 
 
 
