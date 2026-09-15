550c55c94fe0ffa0e025a77ab5db3ffa853a171d
Kseniya <baharevaxen@yandex.ru>
2026-09-15 17:06:35 +0300

    home+base.html

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from app.api.company_routes import router_companies
 from app.api.ai_routes import router_ai
 from app.api.task_routes import router_tasks
@@ -17,6 +20,26 @@ app = FastAPI(
     description="KSENIA TEST 0",
     version="0.1.0",
 )
+
+
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static",
+)
+
+
+
+@app.get("/", include_in_schema=False)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+    )
 app.include_router(router_ai)
 app.include_router(router_companies)
 app.include_router(router_tasks)
