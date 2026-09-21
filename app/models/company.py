1f573d30292ab5f9b9d9b16f52be73464e80e6ad
Kseniya <baharevaxen@yandex.ru>
2026-09-21 13:11:31 +0300

    adding class status + alembic

--- app/models/company.py
+++ app/models/company.py
@@ -1,6 +1,17 @@
-from sqlalchemy import String, BigInteger
+from sqlalchemy import String, BigInteger, Enum as SAEnum
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database.base import Base
+from enum import Enum
+
+
+
+
+class Progress(str,Enum):
+    ACTIVE = "active"
+    ARCHIVED = "archived"
+    REJECTED = "rejected"
+    TRANSFERRED = "transferred"
+
 
 
 class Company(Base):
@@ -32,3 +43,4 @@ class Company(Base):
     profit_growth_3: Mapped[float | None] = mapped_column(nullable=True)
     ai_priority: Mapped[int | None] = mapped_column(nullable=True, index=True)
     ai_risk: Mapped[str | None] = mapped_column(String(20), nullable=True, index=True)
+    progress: Mapped[Progress] = mapped_column(SAEnum(Progress,name="company_progress",values_callable=lambda enum: [e.value for e in enum],),default=Progress.ACTIVE,server_default="active",index=True,)
